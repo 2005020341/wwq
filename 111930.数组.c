0f4caefd3a6157889f3007d5903a319b23568da5
2005020341 <74294683+2005020341@users.noreply.github.com>
2020-11-19 20:16:30 +0800

    Update 111930.数组.c

--- 111930.数组.c
+++ 111930.数组.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 void main(){
-	int temp[]={2020,2021,2022,2022};
+	int atm[]={2020,2021,2022,2022};
 	for(int a=0;a<4;a++)
 	{
-		printf("%d\n",temp[a]);
+		printf("%d\n",atm[a]);
 	}
 }
