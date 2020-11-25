7b9aca1a77ff7cf0cc46538c3d1f629c5fdd6e10
2005020341 <74294683+2005020341@users.noreply.github.com>
2020-11-25 09:46:38 +0800

    Create 112502.QS排名查询.app.c

---
+++ 112502.QS排名查询.app.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	scanf("%d",&a);
+	if(a==1)
+	{
+		printf("北京大学");
+	}
+	else if(a==2)
+    {printf("清华大学");
+	}
+	else if(a==3)
+	{printf("复旦大学");}
+	else if(a==4)
+    {printf("浙江大学");
+	}
+	else if(a==5)
+    {printf("广西大学");
+	}
+	else if(a==6)
+    {printf("武汉大学");
+	}
+	else if(a==7)
+    {printf("中国科技大学");
+	}
+	else if(a==8)
+    {printf("人民大学");
+	}	
+	else if(a=9)
+    {printf("南京大学");
+	}	
+	else if(a=10)
+    {printf("杭州大学");
+	}
+	else
+	{	printf("排名太靠后，请输入1-10");
+	}
+}
