1b6c504f8fef15d8e14b2a999f3d42ab6bf1b337
2005020341 <74294683+2005020341@users.noreply.github.com>
2020-11-25 16:53:14 +0800

    Create 112503.模拟延时程序.app.c

---
+++ 112503.模拟延时程序.app.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<windows.h>
+void main(){
+	printf("来首歌\n");
+	Sleep(3000);
+	printf("叹郁孤\n");
+    Sleep(3000);
+	printf("求佛入魔\n");
+	Sleep(3000);
+	printf("黄沙\n");
+    Sleep(3000);
+	printf("生僻字\n");
+}
