c1eb3b1b12a8246ebdb657116047ae284315b4e9
2005020341 <74294683+2005020341@users.noreply.github.com>
2020-11-26 18:31:34 +0800

    Update 112505.小霸王猜数字.app.c

--- 112505.小霸王猜数字.app.c
+++ 112505.小霸王猜数字.app.c
@@ -1,1 +1,21 @@
+#include<stdio.h>
+#include<windows.h>
+void main(){
+	int mun=6;
+	int s;
+	do
+	{    scanf("%d",&s);
 
+	if(s==mun)
+	{printf("恭喜你答对了\n");
+	}
+	else if(s>mun){
+	printf("你猜的太大了\n");
+	}
+else if(s<mun){
+	printf("你猜的太小了\n");
+	}}
+	while(1);
+
+system("pause");
+}
