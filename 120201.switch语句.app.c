5653ae13ddbd592235bef34c2016e1cb571e30c7
2005020341 <74294683+2005020341@users.noreply.github.com>
2020-12-02 08:54:02 +0800

    Create 120201.switch语句.app.c

---
+++ 120201.switch语句.app.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main(){
+int n;
+scanf("%d",&n);
+switch(n){
+case 1:printf("北京大学\n");break;
+case 2:printf("上海大学\n");break;
+case 3:printf("济南大学\n");break;
+case 4:printf("清华大学\n");break;
+case 5:printf("浙江大学\n");break;
+case 6:printf("南京大学\n");break;
+case 7:printf("东华大学\n");break;
+case 8:printf("长春大学\n");break;
+case 9:printf("东北大学\n");break;
+case 10:printf("广西大学\n");break;
+case 11:printf("杭州大学\n");break;
+case 12:printf("同济大学\n");break;
+case 13:printf("天津大学\n");break;
+case 14:printf("复旦大学\n");break;
+case 15:printf("武汉大学\n");break;
+case 16:printf("山东大学\n");break;
+case 17:printf("湖南大学\n");break;
+case 18:printf("厦门大学\n");break;
+case 19:printf("重庆大学\n");break;
+case 20:printf("南昌大学\n");break;
+default:printf("输入有误，请重新输入\n");
+}
+}
